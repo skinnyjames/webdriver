d0196dab6fda31e8d74846b131e2740ff0c94b77
Sean Gregory <seangregory@ThinkPad-L13-Yoga-63e2b80e.localdomain>
2021-09-05 09:55:22 -0400

    debug

--- src/webdriver/commands/session.cr
+++ src/webdriver/commands/session.cr
@@ -3,6 +3,7 @@ module Webdriver
     module Session
       def start_session(capabilities : Capabilities)
         res = HTTP::Client.post("#{base_url}/session",  body: { capabilities: capabilities.to_h }.to_json)
+        puts res.body
         body = JSON.parse(res.body)
         @session_id = body["value"]["sessionId"].as_s
         body
