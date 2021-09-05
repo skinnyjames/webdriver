233c784e43178f655349e50bf939af2a199c3737
Sean Gregory <seangregory@ThinkPad-L13-Yoga-63e2b80e.localdomain>
2021-09-05 10:00:48 -0400

    desired caps

--- src/webdriver/commands/session.cr
+++ src/webdriver/commands/session.cr
@@ -2,7 +2,7 @@ module Webdriver
   module Commands
     module Session
       def start_session(capabilities : Capabilities)
-        res = HTTP::Client.post("#{base_url}/session",  body: { capabilities: capabilities.to_h }.to_json)
+        res = HTTP::Client.post("#{base_url}/session",  body: { capabilities: capabilities.to_h, desired_capabilities: capabilities.to_h }.to_json)
         puts res.body
         body = JSON.parse(res.body)
         @session_id = body["value"]["sessionId"].as_s
