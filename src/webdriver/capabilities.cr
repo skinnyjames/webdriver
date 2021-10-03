bff50beb17b7e343b2d4350abca1e5e26842d524
Sean Gregory <seangregory@ThinkPad-L13-Yoga-63e2b80e.localdomain>
2021-10-03 11:22:43 -0400

    make it green

--- src/webdriver/capabilities.cr
+++ src/webdriver/capabilities.cr
@@ -1,7 +1,7 @@
 module Webdriver
   abstract class Capabilities
-    def self.default(browser : Symbol)
-      return Chrome.new(browser_name: "chrome", browser_version: "93.0", platform_name: "Linux", platform_version: "", ssl: true)
+    def self.default(browser : Symbol, args : Array(String) = [] of String)
+      return Chrome.new(browser_name: "chrome", browser_version: "93.0", platform_name: "Linux", platform_version: "", ssl: true, args: args)
     end
 
     def to_h
@@ -15,21 +15,37 @@ module Webdriver
       browser_version: String,
       platform_name: String,
       platform_version: String,
-      ssl: Bool
+      ssl: Bool,
+      args: Array(String)
     )
 
     def initialize(**opts)
       @opts = opts
     end
 
-    def to_h
-      Hash(String, String | Bool | Hash(String, Array(String))){
-        "browserName" => @opts[:browser_name],
-        "browserVersion" => @opts[:browser_version],
-        "platformName" => @opts[:platform_name],
-        "platformVersion" => @opts[:platform_version],
-        "acceptSslCerts" => @opts[:ssl],
-      }
+    def args
+      @opts[:args]
+    end
+
+    def to_h(remote)
+      if remote 
+        Hash(String, Hash(String, String | Hash(String, Array(String)))){
+          "alwaysMatch" => { 
+            "browserName" => @opts[:browser_name],
+            "goog:chromeOptions" => { 
+              "args" => @opts[:args]
+            } 
+          }
+        }
+      else
+        Hash(String, String | Array(String) | Bool){
+          "browserName" => @opts[:browser_name],
+          "browserVersion" => @opts[:browser_version],
+          "platformName" => @opts[:platform_name],
+          "platformVersion" => @opts[:platform_version],
+          "acceptSslCerts" => @opts[:ssl],
+        }
+      end
     end
   end
 end
--- src/webdriver/commands/session.cr
+++ src/webdriver/commands/session.cr
@@ -1,12 +1,11 @@
 module Webdriver
   module Commands
     module Session
-      def start_session(capabilities : Capabilities)
+      def start_session(capabilities : Capabilities, remote : Bool)
         headers = HTTP::Headers{"Content-Type" => "application/json", "Connection" => "Keep-Alive", "Accept-Encoding" => "gzip" }
-        json = { capabilities: { alwaysMatch: { browserName: "chrome", "goog:chromeOptions": { "args": ["no-sandbox","headless", "disable-dev-shm-usage"]} }} }.to_json
+        json = { capabilities: capabilities.to_h(remote) }.to_json
         res = HTTP::Client.post("#{base_url}/session",  body: json, headers: headers)
         body = JSON.parse(res.body)
-        pp body
         @session_id = body["value"]["sessionId"].as_s
       end
   
