28f773be0b32130a0cca4b63fae0121a23a95899
Sean Gregory <seangregory@ThinkPad-L13-Yoga-63e2b80e.localdomain>
2021-10-03 10:00:36 -0400

    start remote session test

--- src/webdriver/commands/session.cr
+++ src/webdriver/commands/session.cr
@@ -2,13 +2,11 @@ module Webdriver
   module Commands
     module Session
       def start_session(capabilities : Capabilities)
-        puts capabilities.to_h
-        res = HTTP::Client.post("#{base_url}/session",  body: { capabilities: capabilities.to_h }.to_json)
+        headers = HTTP::Headers{"Content-Type" => "application/json", "Connection" => "Keep-Alive", "Accept-Encoding" => "gzip" }
+        json = { capabilities: { alwaysMatch: { browserName: "chrome" }} }.to_json
+        res = HTTP::Client.post("#{base_url}/session",  body: json, headers: headers)
         body = JSON.parse(res.body)
-        puts res.body
         @session_id = body["value"]["sessionId"].as_s
-        puts @session_id
-        body
       end
   
       def delete_session
