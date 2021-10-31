9ab881073d15428fa93c747339caac95804a2b76
Sean Gregory <seangregory@ThinkPad-L13-Yoga-63e2b80e.localdomain>
2021-10-31 13:31:29 -0400

    async spec

--- src/webdriver/wait.cr
+++ src/webdriver/wait.cr
@@ -2,14 +2,26 @@ require "./element"
 module Webdriver
   class TimeoutException < Exception; end
   module Wait
+    def self.wait_while(interval : Float, timeout : Int32, object : Object = nil, &block)
+      wait(interval: interval, timeout: timeout, object: object, negate: true) do |object|
+        yield object
+      end
+    end
+
     def self.wait_until(interval : Float, timeout : Int32, object : Object = nil, &block)
+      wait(interval: interval, timeout: timeout, object: object, negate: false) do |object|
+        yield object
+      end
+    end
+
+    protected def self.wait(interval : Float, timeout : Int32, object : Object = nil, negate : Bool = false, &block)
       time = Time.local
       while (Time.local - time).seconds < timeout
         begin
           result = yield(object)
-          return result if !!result
+          return object if (negate ? !result : !!result)
         rescue ex : Exception
-          puts ex
+          Log.info { ex }
           sleep interval
         end
       end
